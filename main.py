16e8a0104bb1d9d914aeff3775e9ee18f8c46fce
Sebastián Del Rincón <sebasdelr@gmail.com>
2022-03-04 10:35:28 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+# We will need random to create array
+import random
+
+
+# Creating a basic window
+window = Tk()
+window.title("Algorithms Visualizer")
+window.maxsize(1000, 700)
+window.config(bg = '#FFFFFF')
+
+window.mainloop()
