08f54cd770825cd1d2dfe03849f0c31be3076642
Sebastián Del Rincón <sebasdelr@gmail.com>
2022-03-08 11:17:44 -0700

    User interface

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from tkinter import *
 from tkinter import ttk
 
-# We will need random to create array
 import random
 
-# Creating a basic window
+#Variables
+WHITE = '#FFFFFF'
+
+#Creating a basic window
 window = Tk()
 window.title("Algorithms Visualizer")
 window.maxsize(1000, 700)
-window.config(bg = '#FFFFFF')
+window.config(bg = WHITE)
+
+
+
+#User Interface
+UI_frame = Frame(window, width=900, bg=WHITE)
+UI_frame.grid(row=0, column=0, padx=10, pady=5)
+
+
+#Canvas
+main_canvas = Canvas(window, width=800, height=400, bg=WHITE)
+main_canvas.grid(row=1, column=0, padx=10, pady=5)
 
 window.mainloop()
